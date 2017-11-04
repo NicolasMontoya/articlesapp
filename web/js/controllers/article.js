de919dfef55ed4eccc94d9132ee554f2bfe2cca5
Nicolas Montoya <nicolas96.un@gmail.com>
2017-11-03 23:14:40 -0500

    Added styles and first functions in controllers js

--- web/js/controllers/article.js
+++ web/js/controllers/article.js
@@ -2,39 +2,36 @@ var app=angular.module("myapp",['ngAnimate']);
 
 
 
-app.controller('ArticleCtrl',function(){
+app.controller('ArticleCtrl',function($scope,$http){
     
     var ctrl = this;
     ctrl.text = "Hola";
-    crtl.hola = "ss";
+    ctrl.hola = "ss";
+    ctrl.save = function(){
+        var fd = new FormData();
+        var files = document.getElementById('file').files[0];
+        fd.append('file',files);
+        $http({
+            method: 'GET',
+            url: 'https://www.example.com/api/v1/page',
+            data: {test:'prueba'}
+         });
+        
+    };
+    ctrl.start = function(){
+        ctrl.loading = false;
+        setTimeout(function(){
+            $scope.$apply(function(){
+                ctrl.loading = true;
+            });
+            console.log(ctrl.loading);
+            var element = document.getElementById("loading");
+            element.parentNode.removeChild(element);
+        },100); 
+    };
+
+    $scope.readyCallback = function() {
+        alert("ok");
+    }
 });
 
-
-
-app.directive(
-    "mAppLoading",
-    function( $animate ) {
-        // Return the directive configuration.
-        return({
-            link: link,
-            restrict: "C"
-        });
-        // I bind the JavaScript events to the scope.
-        function link( scope, element, attributes ) {
-            // Due to the way AngularJS prevents animation during the bootstrap
-            // of the application, we can't animate the top-level container; but,
-            // since we added "ngAnimateChildren", we can animated the inner
-            // container during this phase.
-            // --
-            // NOTE: Am using .eq(1) so that we don't animate the Style block.
-            $animate.leave( element.children().eq( 1 ) ).then(
-                function cleanupAfterAnimation() {
-                    // Remove the root directive element.
-                    element.remove();
-                    // Clear the closed-over variable references.
-                    scope = element = attributes = null;
-                }
-            );
-        }
-    }
-);
