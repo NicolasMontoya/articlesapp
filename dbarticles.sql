0c32ef0898247b3896db695d915bde78519df22c
Nicolas Montoya <nicolas96.un@gmail.com>
2017-11-04 22:09:45 -0500

    Change correct insert data in sql SCRIPT

--- dbarticles.sql
+++ dbarticles.sql
@@ -31,9 +31,10 @@ CREATE TABLE `article` (
 --
 
 INSERT INTO `article` (`id`, `title`, `message`, `img`) VALUES
-(1, 'Televisor 32\'', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate ut est vel sagittis. Donec feugiat maximus dui vitae interdum. Duis vel commodo nibh. Quisque posuere orci felis, a euismod tortor tempus et.', 'uploads/tv.jpeg'),
-(2, 'Iphone X', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate ut est vel sagittis. Donec feugiat maximus dui vitae interdum. Duis vel commodo nibh. Quisque posuere orci felis, a euismod tortor tempus et.', 'uploads/iphonex.jpg'),
-(3, 'Surface Pro 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate ut est vel sagittis. Donec feugiat maximus dui vitae interdum. Duis vel commodo nibh. Quisque posuere orci felis, a euismod tortor tempus et.', 'uploads/surface.jpg');
+(1, 'Oficinas 2017 - Poblado', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate ut est vel sagittis. Donec feugiat maximus dui vitae interdum. Duis vel commodo nibh. Quisque posuere orci felis, a euismod tortor tempus et.', 'uploads/mesa.jpg'),
+(2, 'Nuevo Iphone X', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate ut est vel sagittis. Donec feugiat maximus dui vitae interdum. Duis vel commodo nibh. Quisque posuere orci felis, a euismod tortor tempus et.', 'uploads/iphonex.jpg'),
+(3, 'Sale Surface Pro 2', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate ut est vel sagittis. Donec feugiat maximus dui vitae interdum. Duis vel commodo nibh. Quisque posuere orci felis, a euismod tortor tempus et.', 'uploads/surface.jpg'),
+(4, 'Samsung Galaxy S8 - El mas rapido', 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut vulputate ut est vel sagittis. Donec feugiat maximus dui vitae interdum. Duis vel commodo nibh. Quisque posuere orci felis, a euismod tortor tempus et.', 'uploads/galaxy.jpg');
 
 --
 -- Índices para tablas volcadas
